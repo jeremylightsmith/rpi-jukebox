cd202596d425504efdabd28eea520223897c1720
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2015-11-08 16:36:11 +0000

    typos

--- Jukebox/card_reader.py
+++ Jukebox/card_reader.py
@@ -3,6 +3,8 @@ import time
 import sys
 import threading
 
+KEY_ENTER = 28
+
 class CardReader(threading.Thread):
   def __init__(self, bus):
     super(CardReader, self).__init__()
@@ -10,7 +12,7 @@ class CardReader(threading.Thread):
     self.setDaemon(True)
 
   def run(self):
-    self.infile_path = "/dev/input/by-id/usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd"
+    infile_path = "/dev/input/by-id/usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd"
     
     #long int, long int, unsigned short, unsigned short, unsigned int
     FORMAT = 'llHHI'
@@ -19,18 +21,27 @@ class CardReader(threading.Thread):
     in_file = open(infile_path, "rb")
 
     event = in_file.read(EVENT_SIZE)
+    id = ""
 
     while event:
-        (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+      (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
 
-        if type != 0 or code != 0 or value != 0:
-            print("Event type %u, code %u, value: %u at %d, %d" % \
-                (type, code, value, tv_sec, tv_usec))
+      if type == 1 and value == 1:
+        if code == KEY_ENTER:
+          print "submiting"
+          print id
+          self.bus.put(["card_read", id])
+          id = ""
+
+        elif code == 11:
+          id += `0`
+          print "id = " + id
+
         else:
-            # Events with code, type and value == 0 are "separator" events
-            print("===========================================")
+          id += `code - 1`
+          print "id = " + id
 
-        event = in_file.read(EVENT_SIZE)
+      event = in_file.read(EVENT_SIZE)
 
     in_file.close()
     # while True:
