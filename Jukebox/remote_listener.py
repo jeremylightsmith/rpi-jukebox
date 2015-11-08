e829d24a59f2b2c6a513ef13be9d1d09bff39b03
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2015-11-08 09:11:46 -0800

    working on remote listener

--- Jukebox/remote_listener.py
+++ Jukebox/remote_listener.py
@@ -9,10 +9,32 @@ class RemoteListener(threading.Thread):
     self.bus = bus
     self.setDaemon(True)
 
-
   def run(self):
-    while True:
-      print("next")
-      self.bus.put(["next"])
-      time.sleep(17)
+    infile_path = "/dev/input/by-id/usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd"
+    
+    #long int, long int, unsigned short, unsigned short, unsigned int
+    FORMAT = 'llHHI'
+    EVENT_SIZE = struct.calcsize(FORMAT)
 
+    in_file = open(infile_path, "rb")
+
+    event = in_file.read(EVENT_SIZE)
+    id = ""
+
+    while event:
+      (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
+
+      if type == 1 and value == 1:
+        if code == KEY_ENTER:
+          self.bus.put(["card_read", id])
+          id = ""
+
+        elif code == 11:
+          id += `0`
+
+        else:
+          id += `code - 1`
+
+      event = in_file.read(EVENT_SIZE)
+
+    in_file.close()
